548ce7e64412c997afefac844195b67c87f21f78
Shivang Tiwari <shivang.tiwari@hexaviewtech.com>
2025-04-06 22:49:10 +0530

    feature-YST-9-auth-routes

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
 import { connectDB1, connectDB2 } from "./src/db_connect/dbConnect";
+import router from "./src/routes";
 
 const app: Application = express();
 app.use(express.json());
 dotenv.config();
 connectDB1();
 connectDB2();
+app.use('/api', router);
 app.listen(3030, () => {
     console.log('Server running on port 3030');
 });
