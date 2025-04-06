bd4cf276dfdcf00ea26c13b43d189ddd71d07b66
Shivang Tiwari <shivang.tiwari@hexaviewtech.com>
2025-04-06 13:26:08 +0530

    Initial setup talker BE

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
 import express, { Application } from "express";
+import dotenv from "dotenv";
+import { connectDB1, connectDB2 } from "./src/db_connect/dbConnect";
 
 const app: Application = express();
+app.use(express.json());
+dotenv.config();
+connectDB1();
+connectDB2();
 app.listen(3030, () => {
     console.log('Server running on port 3030');
 });
---
+++ src/db_connect/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { Sequelize } from "sequelize";
+
+const connectDB1 = async () => {
+  const sequlize = new Sequelize("talker", "root", "9643@Hexaview", {
+    host: "localhost",
+    dialect: "mysql",
+  });
+  try {
+    await sequlize.authenticate();
+    console.log("MySQL Connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const connectDB2 = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://shivang1802tiwari:${process.env.DB2PASS}@cluster0.d8blyju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { connectDB1, connectDB2 };
