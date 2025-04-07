3ebd431c7b9b8bb24bbf47e641b2506fb25a1547
Shivang Tiwari <shivang.tiwari@hexaviewtech.com>
2025-04-07 14:35:25 +0530

    ohh sorry

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,9 +1,7 @@
 import {Response, Request} from 'express';
 
 const signUpController = (req: Request, res: Response) => {
-    if(!req.body.email){
-        res.status(400).send({error: 'Email not provided!'})
-    }
+    console.log(req.body);
     res.status(200).send({mesage: "Sign up route working"});
 }
 
@@ -14,16 +12,12 @@ const signInController = (req: Request, res: Response) => {
 
 const forgotPasswordController = (req: Request, res: Response) => {
     console.log(req.body);
-    res.status(200).send({meesage: 'Forgot route working'});
+    res.send(200).send({meesage: 'Forgot route working'});
 }
 
 const verifyemailController = (req: Request, res: Response) => {
     console.log(req.body);
     res.status(200).send({message: 'Verify email route working'});
-}
-
-const yuktaBaby = () => {
-    console.log("Yukta baby");
 }
 
 export {signUpController, signInController, forgotPasswordController, verifyemailController}
