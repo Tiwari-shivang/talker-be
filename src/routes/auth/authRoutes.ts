6eb9ffffb92b10e2f9d42b06f877f70442164cb7
Shivang Tiwari <shivang.tiwari@hexaviewtech.com>
2025-04-27 16:26:51 +0530

    Verify user API creation done

--- src/routes/auth/authRoutes.ts
+++ src/routes/auth/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { forgotPasswordController, signInController, signUpController, verifyemailController } from '../../controllers/authControllers';
+import { forgotPasswordController, signInController, signUpController, verifyemailController, verifyUserController } from '../../controllers/authControllers';
 const authRouter = express.Router();
 
 authRouter.post('/signup', signUpController);
 authRouter.post('/signin', signInController);
 authRouter.post('/forgot', forgotPasswordController);
 authRouter.post('/verify-email', verifyemailController);
+authRouter.put('/verify-user', verifyUserController);
 
 export default authRouter;
